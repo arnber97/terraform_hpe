26cf9ab9476b7e3753c1d9850a94f4c2bb667643
arnber97 <47865295+arnber97@users.noreply.github.com>
2025-02-06 17:32:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,79 +1,8 @@
-terraform {
-  required_providers {
-    hpegl = {
-      source  = "hpe/hpegl"
-      version = "0.4.15"
-    }
-  }
-}
-#Environnement du provider
-provider "hpegl" {
-  vmaas {
-    location   = "HPE"
-    space_name = "Hosted-Trial-TR2-Space"
-  }
-}
-
-# Retrieve Cloud ID
-data "hpegl_vmaas_cloud" "cloud" {
-  name = "HPE GreenLake VMaaS Cloud-Trial2"
-}
-
-# And a few networks
-data "hpegl_vmaas_network" "blue_net" {
-     name = "Blue-Net"
-   }
-data "hpegl_vmaas_network" "green_net" {
-     name = "Green-Net"
-   }
-
-# And a group
-data "hpegl_vmaas_group" "default_group" {
-  name = "GreenBoat"
-}
-
-# Locate a resource pool
-data "hpegl_vmaas_resource_pool" "cl_resource_pool" {
-     cloud_id = data.hpegl_vmaas_cloud.cloud.id
-     name = "ComputeResourcePool"
-   }
-
-# A layout
-data "hpegl_vmaas_layout" "vmware" {
-     name = "Vmware VM"
-     instance_type_code = "VMware"
-}
-
-#A Plan
-data "hpegl_vmaas_plan" "g1-small" {
-	name = "G1-Small"
-}
-
-# A Template
-data "hpegl_vmaas_template" "rhel9ng" {
-	name = "master-NG-RHEL9-beta-v0"
-}
-
-#An Environnement
-data "hpegl_vmaas_environment" "dev" {
-	name = "Dev"
-}
-
-#A Group
-data "hpegl_vmaas_group" "greenboat"  {
-	name = "GreenBoat"
-}
-
-# A Folder
-data "hpegl_vmaas_cloud_folder" "folder" {
-	cloud_id = data.hpegl_vmaas_cloud.cloud.id
-	name = "ComputeFolder"
-}
 #########################################
 # Ressources
 #########################################
-resource "hpegl_vmaas_instance" "TF1-ABR" {
-	name = "tf1-abr"
+resource "hpegl_vmaas_instance" "TF2-ABR" {
+	name = "tf2-abr"
 	layout_id = data.hpegl_vmaas_layout.vmware.id
  	instance_type_code = data.hpegl_vmaas_layout.vmware.instance_type_code
 	cloud_id = data.hpegl_vmaas_cloud.cloud.id
@@ -93,10 +22,10 @@ resource "hpegl_vmaas_instance" "TF1-ABR" {
 		datastore_id = "auto"
 		}
 	config {
-		template_id = data.hpegl_vmaas_template.rhel9ng.id
+		template_id = data.hpegl_vmaas_template.centos-7.id
 		resource_pool_id = data.hpegl_vmaas_resource_pool.cl_resource_pool.id
 		no_agent = false
-		asset_tag = "TF VM"
+		asset_tag = "TF2 VM"
 		folder_code = data.hpegl_vmaas_cloud_folder.folder.code
 		}
 	power = "poweron"
