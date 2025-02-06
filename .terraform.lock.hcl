7b6a959b2514ac18828207f44e2b287fca0ba01c
Arnaud B <arnaud.berder@gmail.com>
2025-02-06 22:01:22 +0530

    Add main.tf

---
+++ .terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hpe/hpegl" {
+  version     = "0.4.15"
+  constraints = "0.4.15"
+  hashes = [
+    "h1:ZFLtrc58ON69CI6Y1kSszBcdt6aTWMdTYTNo+za9XO4=",
+  ]
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    hpegl = {
+      source  = "hpe/hpegl"
+      version = "0.4.15"
+    }
+  }
+}
 #Environnement du provider
 provider "hpegl" {
   vmaas {
@@ -19,7 +27,7 @@ data "hpegl_vmaas_network" "green_net" {
      name = "Green-Net"
    }
 
-# Group
+# And a group
 data "hpegl_vmaas_group" "default_group" {
   name = "GreenBoat"
 }
@@ -30,52 +38,68 @@ data "hpegl_vmaas_resource_pool" "cl_resource_pool" {
      name = "ComputeResourcePool"
    }
 
-# Layout
+# A layout
 data "hpegl_vmaas_layout" "vmware" {
      name = "Vmware VM"
      instance_type_code = "VMware"
 }
 
-# Plan
+#A Plan
 data "hpegl_vmaas_plan" "g1-small" {
 	name = "G1-Small"
 }
-data "hpegl_vmaas_plan" "g1-medium" {
-	name = "G1-Medium"
-}
-data "hpegl_vmaas_plan" "g1-large" {
-	name = "G1-Large"
-}
 
-# Template
+# A Template
 data "hpegl_vmaas_template" "rhel9ng" {
 	name = "master-NG-RHEL9-beta-v0"
 }
-data "hpegl_vmaas_template" "ubuntu-20" {
-	name = "ubuntu-20"
-}
-data "hpegl_vmaas_template" "centos-7" {
-	name = "vanilla-centos-7-x86_64-18062020"
-}
 
-
-# Environnement
+#An Environnement
 data "hpegl_vmaas_environment" "dev" {
 	name = "Dev"
 }
-data "hpegl_vmaas_environment" "test" {
-	name = "Test"
-}
-data "hpegl_vmaas_environment" "production" {
-	name = "Production"
-}
-# Group
+
+#A Group
 data "hpegl_vmaas_group" "greenboat"  {
 	name = "GreenBoat"
 }
 
-# Folder
+# A Folder
 data "hpegl_vmaas_cloud_folder" "folder" {
 	cloud_id = data.hpegl_vmaas_cloud.cloud.id
 	name = "ComputeFolder"
 }
+#########################################
+# Ressources
+#########################################
+resource "hpegl_vmaas_instance" "TF1-ABR" {
+	name = "tf1-abr"
+	layout_id = data.hpegl_vmaas_layout.vmware.id
+ 	instance_type_code = data.hpegl_vmaas_layout.vmware.instance_type_code
+	cloud_id = data.hpegl_vmaas_cloud.cloud.id
+	group_id = data.hpegl_vmaas_group.greenboat.id
+	plan_id = data.hpegl_vmaas_plan.g1-small.id
+	network {
+		id = data.hpegl_vmaas_network.green_net.id
+		}
+	volume {
+		name = "root_vol"
+		size = 25
+		datastore_id = "auto"
+		}
+	volume {
+		name = "local_vol"
+		size = 5
+		datastore_id = "auto"
+		}
+	config {
+		template_id = data.hpegl_vmaas_template.rhel9ng.id
+		resource_pool_id = data.hpegl_vmaas_resource_pool.cl_resource_pool.id
+		no_agent = false
+		asset_tag = "TF VM"
+		folder_code = data.hpegl_vmaas_cloud_folder.folder.code
+		}
+	power = "poweron"
+	}
+
+
