b1a741f9fb2770bc3d8ce02561a848247c30ec8b
arnber97 <47865295+arnber97@users.noreply.github.com>
2025-02-07 14:14:25 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,14 +1,47 @@
-variable "vmname" {
-    type = string
-    default = "tf-vm-abr1"
-}
-variable "root_vol-size" {
-	default = 25
-	}
-variable "local_vol-size" {
-	default = 5
-	}
+###################################
+Default VAR
+###################################
 variable "tag" {
 	type = string
 	default = "Terraform VM ABR"
+	}
+
+###################################
+WEB Server VAR
+###################################
+variable "vmname-web" {
+    type = string
+    default = "tf-vm-web"
+}
+variable "root_vol-size-web" {
+	default = 25
+	}
+variable "local_vol-size-web" {
+	default = 5
+	}
+###################################
+APP Server VAR
+###################################
+variable "vmname-app" {
+    type = string
+    default = "tf-vm-app"
+}
+variable "root_vol-size-app" {
+	default = 25
+	}
+variable "local_vol-size-app" {
+	default = 5
+	}
+###################################
+BDD Server VAR
+###################################
+variable "vmname-bdd" {
+    type = string
+    default = "tf-vm-bdd"
+}
+variable "root_vol-size-bdd" {
+	default = 25
+	}
+variable "local_vol-size-bdd" {
+	default = 5
 	}
