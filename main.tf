82c7d8398d883ff24721132011edce0cd3780a6a
arnber97 <47865295+arnber97@users.noreply.github.com>
2025-02-07 14:10:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,4 +31,62 @@ resource "hpegl_vmaas_instance" "TF2-ABR" {
 	power = "poweron"
 	}
 
+resource "hpegl_vmaas_instance" "TF3-ABR" {
+	name = var.vmname-app
+	layout_id = data.hpegl_vmaas_layout.vmware.id
+ 	instance_type_code = data.hpegl_vmaas_layout.vmware.instance_type_code
+	cloud_id = data.hpegl_vmaas_cloud.cloud.id
+	group_id = data.hpegl_vmaas_group.greenboat.id
+	plan_id = data.hpegl_vmaas_plan.g1-small.id
+	network {
+		id = data.hpegl_vmaas_network.green_net.id
+		}
+	volume {
+		name = "root_vol"
+		size = var.root_vol-size-app
+		datastore_id = "auto"
+		}
+	volume {
+		name = "local_vol"
+		size = var.local_vol-size-app
+		datastore_id = "auto"
+		}
+	config {
+		template_id = data.hpegl_vmaas_template.centos-7.id
+		resource_pool_id = data.hpegl_vmaas_resource_pool.cl_resource_pool.id
+		no_agent = false
+		asset_tag = var.tag
+		folder_code = data.hpegl_vmaas_cloud_folder.folder.code
+		}
+	power = "poweron"
+	}
 
+resource "hpegl_vmaas_instance" "TF4-ABR" {
+	name = var.vmname-bdd
+	layout_id = data.hpegl_vmaas_layout.vmware.id
+ 	instance_type_code = data.hpegl_vmaas_layout.vmware.instance_type_code
+	cloud_id = data.hpegl_vmaas_cloud.cloud.id
+	group_id = data.hpegl_vmaas_group.greenboat.id
+	plan_id = data.hpegl_vmaas_plan.g1-small.id
+	network {
+		id = data.hpegl_vmaas_network.green_net.id
+		}
+	volume {
+		name = "root_vol"
+		size = var.root_vol-size-bdd
+		datastore_id = "auto"
+		}
+	volume {
+		name = "local_vol"
+		size = var.local_vol-size-bdd
+		datastore_id = "auto"
+		}
+	config {
+		template_id = data.hpegl_vmaas_template.centos-7.id
+		resource_pool_id = data.hpegl_vmaas_resource_pool.cl_resource_pool.id
+		no_agent = false
+		asset_tag = var.tag
+		folder_code = data.hpegl_vmaas_cloud_folder.folder.code
+		}
+	power = "poweron"
+	}
